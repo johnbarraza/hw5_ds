d2cb18ec412d4a1bc29163109654f962b03f5782
tgcarrero-code <tg.carreror@alum.up.edu.pe>
2026-06-10 19:22:32 -0500

    Update mcp_server.py

--- mef_subnational_efficiency_mcp/src/mcp_server.py
+++ mef_subnational_efficiency_mcp/src/mcp_server.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1lwtooi-wnueERP1AWmIxuW1sLX2LUnm_
 """
 
-!pip install fastmcp -q
 from __future__ import annotations
 
 import io
